50741f780c5edabb2a8351193b33dabf974aee43
Zanuardi Novanda <zanuardinovanda@gmail.com>
2020-10-24 19:46:55 +0700

    delete unnecessary code

--- routes/profile/getProfile.js
+++ routes/profile/getProfile.js
@@ -3,21 +3,14 @@ const getData = require("../../controllers/getController")
 const app = express.Router()
 
 app.get("/profile", (req, res) => {
-
     const query = req.query
-
     const result = getData('users', query)
-
     if (!result) {
         res.status(404).send('Data not found')
         return
-
     } else {
         res.send(result)
     }
-    res.send(getData('users'))
-
-
 })
 
 module.exports = app
