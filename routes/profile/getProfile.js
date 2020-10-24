06d19af15945b2d05d701e2659d72b8a9e5e3bb0
Zanuardi Novanda <zanuardinovanda@gmail.com>
2020-10-24 18:15:32 +0700

    fix profile route

--- routes/profile/getProfile.js
+++ routes/profile/getProfile.js
@@ -1,13 +1,23 @@
-const express = require("express");
-const getData = require("../../controllers/getController");
-const uid = require("uid");
-const app = express.Router();
+const express = require("express")
+const getData = require("../../controllers/getController")
+const app = express.Router()
 
 app.get("/profile", (req, res) => {
-    const body = req.body;
-    body.id = uid();
-    const result = getData("users", body);
-    res.send(result);
-});
 
-module.exports = app;
+    const id = req.query.id
+    const dataId = getData('users', id.body)
+
+    if (!dataId) {
+        res.status(404).send('Data not found')
+        return
+
+    } else if (dataId) {
+        res.send(dataId)
+    } else {
+        res.send(getData('users'))
+    }
+
+    res.send(result)
+})
+
+module.exports = app
