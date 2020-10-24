9275971d6269f8810def86b80a71f6358bb1f0cd
agussaputran <ntoiagussaputra@gmail.com>
2020-10-24 18:08:10 +0700

    [Add Feature]-addOrders admin route

---
+++ routes/admin/orders/addOrders.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const addData = require("../../../controllers/addController");
+const uid = require("uid");
+const app = express.Router();
+
+app.post("/admin/orders", (req, res) => {
+  const body = req.body;
+  body.id = uid();
+  const result = addData("orders", body);
+  res.send(result);
+});
+
+module.exports = app;
