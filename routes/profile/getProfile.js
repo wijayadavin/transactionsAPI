39cd2aff3b706b609db71b920ae9013eead8be2f
Zanuardi Novanda <zanuardinovanda@gmail.com>
2020-10-24 19:51:39 +0700

    delete unnecessary code

--- routes/profile/getProfile.js
+++ routes/profile/getProfile.js
@@ -5,9 +5,10 @@ const app = express.Router()
 app.get("/profile", (req, res) => {
     const query = req.query
     const result = getData('users', query)
+
     if (!result) {
         res.status(404).send('Data not found')
-        return
+
     } else {
         res.send(result)
     }
