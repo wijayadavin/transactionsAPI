d0e59ff30924a142c35ce38eef83f6e5ee0446bd
Zanuardi Novanda <zanuardinovanda@gmail.com>
2020-10-24 17:11:00 +0700

    add features Profiles

--- routes/profile/getProfile.js
+++ routes/profile/getProfile.js
@@ -2,13 +2,13 @@ const express = require('express')
 const app = express.Router()
 const getData = require("../../controllers/getController");
 
+
 app.get('/profile', (req, res) => {
     const query = req.query
     const id = req.user.id
     query.userId = id
-    const result = db.get('user', query)
+    const result = db.get('users', query)
     res.send(result)
 })
-
 
 module.exports = app
