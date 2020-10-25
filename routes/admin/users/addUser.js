125273c65e8f6ca8d91f0f711b19a4124c4386a1
Davin Wijaya <wijayadavin@gmail.com>
2020-10-26 01:53:02 +0700

    upgrade register to forbid duplicate username

--- routes/admin/users/addUser.js
+++ routes/admin/users/addUser.js
@@ -1,15 +1,21 @@
 
 const express = require('express');
 const addData = require('../../../controllers/addController');
+const getData = require('../../../controllers/getController');
 const uid = require('uid');
 const app = express.Router();
 const auth = require('../../../middlewares/jwtMiddleware');
 
+
 app.post('/admin/users',
     auth.verifyJwt('userLevel: 2'), (req, res) => {
-      const body = req.body;
-      body.id = uid();
-      const result = addData('users', body);
+      isUsernameExist = getData('users', {username: req.body.username});
+
+      if (isUsernameExist && isUsernameExist.length) {
+        res.status(409).send('The same username has exist');
+      }
+      req.body.id = uid();
+      const result = addData('users', req.body);
 
       if (result) {
         res.send(result);
--- routes/auth/registerRoute.js
+++ routes/auth/registerRoute.js
@@ -1,23 +1,29 @@
 const express = require('express');
 const addData = require('../../controllers/addController');
-
+const getData = require('../../controllers/getController');
 const app = express.Router();
 // uid is a id generator library
 // Reference: https://www.npmjs.com/package/uid
 const uid = require('uid');
 
+
 app.post('/auth/register', (req, res) => {
   req.body.id = uid();
   req.body.level = 1;
+  isUsernameExist = getData('users', {username: req.body.username});
+  if (isUsernameExist && isUsernameExist.length) {
+    res.status(409).send('The same username has exist');
+  }
   const result = addData('users', req.body);
 
   if (result) {
     res.send(result);
   } else {
-    // called if request body object key is lacking
+    // called if request body object key is lacking:
     res.status(400).send('Bad request');
   }
 },
 );
+
 
 module.exports = app;
