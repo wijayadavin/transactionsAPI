13b66b9bbcc1cdebb66be180a2399257cbc368fc
Zanuardi Novanda <zanuardinovanda@gmail.com>
2020-10-25 15:12:12 +0700

    Add feature Edit Profile

---
+++ routes/profile/editProfile.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const editData = require("../../../controllers/editController")
+const app = express.Router()
+const auth = require('../../../middlewares/jwtMiddleware')
+
+app.patch("/profile",
+    auth.verifyJwt('userLevel: 1'), (req, res) => {
+        const body = req.body
+        const id = req.query.id
+        const result = editData("users", id, body)
+        res.send(result)
+    })
+
+module.exports = app
--- routes/profile/getProfile.js
+++ routes/profile/getProfile.js
@@ -1,17 +1,19 @@
 const express = require("express")
 const getData = require("../../controllers/getController")
 const app = express.Router()
+const auth = require('../../../middlewares/jwtMiddleware')
 
-app.get("/profile", (req, res) => {
-    const query = req.query
-    const result = getData('users', query)
+app.get("/profile",
+    auth.verifyJwt('userLevel: 1'), (req, res) => {
+        const query = req.query
+        const result = getData('users', query)
 
-    if (!result) {
-        res.status(404).send('Data not found')
+        if (!result) {
+            res.status(404).send('Data not found')
 
-    } else {
-        res.send(result)
-    }
-})
+        } else {
+            res.send(result)
+        }
+    })
 
 module.exports = app
