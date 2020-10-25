d607791e77e491eea63c26180a53061945c04a5a
Davin Wijaya <wijayadavin@gmail.com>
2020-10-25 18:51:16 +0700

    merged create order and get profile

--- routes/orders/addOrders.js
+++ routes/orders/addOrders.js
@@ -1,16 +1,16 @@
-const express = require("express");
-const addData = require("../../controllers/addController");
-const uid = require("uid");
-const auth = require("../../middlewares/jwtMiddleware");
+const express = require('express');
+const addData = require('../../controllers/addController');
+const uid = require('uid');
+const auth = require('../../middlewares/jwtMiddleware');
 const app = express.Router();
 
-app.post("/orders", auth.verifyJwt("userLevel: 1"), (req, res) => {
+app.post('/orders', auth.verifyJwt('userLevel: 1'), (req, res) => {
   const body = req.body;
   body.id = uid();
-  const result = addData("orders", body);
+  const result = addData('orders', body);
 
   if (!result) {
-    res.status(400).send("Wrong body");
+    res.status(400).send('Wrong body');
   } else {
     res.send(result);
   }
--- routes/profile/getProfile.js
+++ routes/profile/getProfile.js
@@ -1,17 +1,16 @@
-const express = require("express")
-const getData = require("../../controllers/getController")
-const app = express.Router()
+const express = require('express');
+const getData = require('../../controllers/getController');
+const app = express.Router();
 
-app.get("/profile", (req, res) => {
-    const query = req.query
-    const result = getData('users', query)
+app.get('/profile', (req, res) => {
+  const query = req.query;
+  const result = getData('users', query);
 
-    if (!result) {
-        res.status(404).send('Data not found')
+  if (!result) {
+    res.status(404).send('Data not found');
+  } else {
+    res.send(result);
+  }
+});
 
-    } else {
-        res.send(result)
-    }
-})
-
-module.exports = app
+module.exports = app;
