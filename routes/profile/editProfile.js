7f1a6e972f4e88a55e23c6cb6d3980f4c05fd5de
Zanuardi Novanda <zanuardinovanda@gmail.com>
2020-10-25 16:26:40 +0700

    add var isUsernameExist (have bug)

--- routes/profile/editProfile.js
+++ routes/profile/editProfile.js
@@ -1,14 +1,23 @@
 const express = require("express")
 const editData = require("../../controllers/editController")
+const getData = require("../../controllers/getController")
 const app = express.Router()
 const auth = require('../../middlewares/jwtMiddleware')
 
 app.patch("/profile",
     auth.verifyJwt('userLevel: 1'), (req, res) => {
         const body = req.body
-        const id = req.query.id
-        const result = editData("users", id, body)
-        res.send(result)
+        const { id, username } = body
+        const isUsernameExist = getData('users', username)
+
+        if (!isUsernameExist) {
+            res.status(409).send('You insert an existed Username')
+        } else {
+            editData('users', id, body)
+            body.id = id
+            res.send(body)
+            return
+        }
     })
 
 module.exports = app
