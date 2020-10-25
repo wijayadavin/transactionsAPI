1cfedea0ec5d6d1baff7b732ba25b54130647c28
agussaputran <ntoiagussaputra@gmail.com>
2020-10-25 15:34:44 +0700

    [Add Auth]-getOrders admin route

--- routes/admin/orders/getOrders.js
+++ routes/admin/orders/getOrders.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const getData = require("../../../controllers/getController");
+const auth = require("../../../middlewares/jwtMiddleware");
 const app = express.Router();
 
-app.get("/admin/orders", (req, res) => {
+app.get("/admin/orders", auth.verifyJwt("userLevel: 2"), (req, res) => {
   const result = getData("orders", req.query);
-  res.send(result);
+  if (result) {
+    res.send(result);
+  } else {
+    res.status(404).send("data not found");
+  }
+  return;
 });
 
 module.exports = app;
