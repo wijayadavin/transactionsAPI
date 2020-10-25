0a6050b12966ccdb314de4c7f753ee49c7d0b2fa
agussaputran <ntoiagussaputra@gmail.com>
2020-10-25 16:20:02 +0700

    [Add Auth]-editOrders admin route

--- routes/admin/orders/editOrders.js
+++ routes/admin/orders/editOrders.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const editData = require("../../../controllers/editController");
+const auth = require("../../../middlewares/jwtMiddleware");
 const app = express.Router();
 
-app.patch("/admin/orders", (req, res) => {
+app.patch("/admin/orders", auth.verifyJwt("userLevel: 2"), (req, res) => {
   const body = req.body;
   const id = req.query.id;
   const result = editData("orders", id, body);
-  res.send(result);
+
+  if (!result) {
+    res.status(400).send("Bad request");
+  } else {
+    res.send(result);
+  }
+  return;
 });
 
 module.exports = app;
