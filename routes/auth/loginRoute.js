19ec501d7c2c58cfa1c4d771c51edbb16f474c5e
Davin Wijaya <wijayadavin@gmail.com>
2020-10-25 16:58:25 +0700

    Revert "add user level feature in login service"
    This reverts commit 5f0908c612625353df4e8abc911d80090dc5e4a6.

--- routes/auth/loginRoute.js
+++ routes/auth/loginRoute.js
@@ -8,8 +8,8 @@ app.post('/auth/login', (req, res) => {
   const result = getData('users', req.body);
 
   if (result) {
-    // if user's level is 2, set a token for admin users:
-    if (req.body.level === 2) {
+    // Set a token for admin users:
+    if (req.body.username == 'admin') {
       const payload = {
         id: result[0].id,
         permissions: 'userLevel: 2',
@@ -20,7 +20,7 @@ app.post('/auth/login', (req, res) => {
 
     // if not admin then set a token for normal users:
     const payload = {
-      id: result[0].id,
+      id: req.body.id,
       permissions: 'userLevel: 1',
     };
     result[0].token = auth.signJwt(payload);
