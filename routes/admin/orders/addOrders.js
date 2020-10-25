04f91971f82889adfa91853c93519ee8d5beca3d
agussaputran <ntoiagussaputra@gmail.com>
2020-10-25 15:59:45 +0700

    [Add Auth]-addOrders admin route

--- routes/admin/orders/addOrders.js
+++ routes/admin/orders/addOrders.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const addData = require("../../../controllers/addController");
 const uid = require("uid");
+const auth = require("../../../middlewares/jwtMiddleware");
 const app = express.Router();
 
-app.post("/admin/orders", (req, res) => {
+app.post("/admin/orders", auth.verifyJwt("userLevel: 2"), (req, res) => {
   const body = req.body;
   body.id = uid();
   const result = addData("orders", body);
-  res.send(result);
+
+  if (!result) {
+    res.status(400).send("Wrong body");
+  } else {
+    res.send(result);
+  }
+  return;
 });
 
 module.exports = app;
