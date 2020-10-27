a6ebe20893059e8d22eeec591f537d6ce1472558
Davin Wijaya <wijayadavin@gmail.com>
2020-10-27 21:02:20 +0700

    migrate profile feature to get by username a path

--- routes/profile/getProfile.js
+++ routes/profile/getProfile.js
@@ -2,14 +2,14 @@ const express = require('express');
 const getData = require('../../controllers/getController');
 const app = express.Router();
 
-app.get('/profile', (req, res) => {
-  const query = req.query;
-  const result = getData('users', query);
-
-  if (!result) {
-    res.status(404).send('Data not found');
-  } else {
+app.get('/u/:username', (req, res) => {
+  const result = getData('users', {username: req.params.username});
+  // get username by username as path, return result if found:
+  if (result && result.length) {
     res.send(result);
+  } else {
+    // or error if not found:
+    res.status(404).send('Data not found');
   }
 });
 
