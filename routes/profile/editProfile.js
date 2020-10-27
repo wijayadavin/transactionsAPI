46096f8e34c04074c9f547b1b65ab80921c78ccb
Davin Wijaya <wijayadavin@gmail.com>
2020-10-27 13:23:49 +0700

    formatting

--- routes/profile/editProfile.js
+++ routes/profile/editProfile.js
@@ -1,32 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const getData = require('../../controllers/getController')
+const getData = require('../../controllers/getController');
 const editData = require('../../controllers/editController');
 const auth = require('../../middlewares/jwtMiddleware');
 
 router.patch('/profile',
     auth.verifyJwt('userLevel: 1'), (req, res) => {
+      // search if username is existed in database
+      const body = req.body;
+      const isUsernameExist = getData('users', body.username);
+      if (isUsernameExist & isUsernameExist.length) {
+        res.status(400).send('Username is existed');
+      }
+      // if passed, users can edit their username
+      req.body.id = req.user.id;
+      const result = editData('users', req.body.id, req.body);
 
-        // search if username is existed in database
-        const body = req.body
-        const isUsernameExist = getData('users', { username: body.username })
-        if (isUsernameExist && isUsernameExist.length) {
-            //if existed, send this
-            res.status(400).send('Username is existed')
-        }
-
-        //if passed, users can edit their username
-        req.query.id = req.user.id
-        const result = editData('users', req.query.id, req.body);
-
-        if (!result) {
-            //if false
-            res.status(400).send('Bad request');
-        } else {
-            //if true
-            res.send(result);
-        }
-        return;
+      if (!result) {
+        // if false
+        res.status(400).send('Bad request');
+      } else {
+        // if true
+        res.send(result);
+      }
+      return;
     });
 
 
