1b9304ef95c72c98d5be52c69915088fa1665e7c
Davin Wijaya <wijayadavin@gmail.com>
2020-10-30 07:31:37 +0700

    remove unused variables

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const auth = require('./middlewares/jwtMiddleware');
 const passport = require('passport');
 const cookieSession = require('cookie-session')
 
