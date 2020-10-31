3346eef9a28b5abb18b65d190065320d2db899ab
Davin Wijaya <wijayadavin@gmail.com>
2020-10-31 23:08:12 +0700

    add getmenus feature to render menus page

--- routes/user/menus/getMenus.js
+++ routes/user/menus/getMenus.js
@@ -4,13 +4,25 @@ const userPermission = require('../../../controllers/userController');
 
 const router = express.Router();
 
-router.get('/menus', userPermission(['user', 'admin']), (req, res) => {
-  const query = req.query;
-  const result = getData('menus', query);
+router.get('/menus',
+// userPermission(['user', 'admin']),
+(req, res) => {
+  const result = getData('menus');
   if (!result) {
-    res.status(404).send('Data not found');
+    return res.status(404).send('Data not found');
   }
-  res.send(result);
+  return res.render('menus', {menus: result});
+});
+
+router.get('/restaurantID/:menuID',
+// userPermission(['user', 'admin']),
+(req, res) => {
+  console.log(req.params.menuID);
+  const result = getData('menus', {id: req.params.menuID})[0];
+  if (!result) {
+    return res.status(404).send('Data not found');
+  }
+  return res.render('menus', {menu: result});
 });
 
 module.exports = router;
